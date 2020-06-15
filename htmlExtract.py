c3f9f0cf030b5a490f334fd2bf66f8c4e24c81b3
agiannop <agianno7811@gmail.com>
2020-06-14 23:05:46 -0400

    Made functions

--- htmlExtract.py
+++ htmlExtract.py
@@ -1,22 +1,30 @@
 # This file is to test out retrieving the URL from a dataset's XML file
 # and then using Boilerpy to grab all the text
 
+# TODO: Develop way to get continuously grab URL and output contents for multiple dataset files
 
 import lxml.etree as ET
 from boilerpy3 import extractors
 extractor = extractors.ArticleExtractor()
 
-# Need to parse the XML file
-file = open("0000001_1.xml")
-tree = ET.parse(file)
-root = tree.getroot()
+def getUrl(dataset):
+    file = open(dataset)
+    tree = ET.parse(file)
+    root = tree.getroot()
 
-# Find the URL which is an attribute (url) of the <Document> tag
-url = root.attrib['url']
-file.close()
+    # Find the URL which is an attribute (url) of the <Document> tag
+    return root.attrib['url']
 
-content = extractor.get_content_from_url(url)
+def parseHtml(url, outputFile):
+    content = extractor.get_content_from_url(url)
 
-file = open("htmlOutput.txt", "a")
-file.write(content)
-file.close()
+    file = open(outputFile, "a")
+    file.write(content)
+    file.close()
+
+def main():
+    url = getUrl("0000001_1.xml")
+    parseHtml(url, "htmlOutput.txt")
+
+if __name__ == "__main__":
+    main()
