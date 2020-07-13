e70f0b324279d4c0cb9d4cdd26186946840b2ceb
agiannop <agianno7811@gmail.com>
2020-07-13 00:25:36 -0400

    Added functionality to search through directories for dataset and output to single text file

--- htmlExtract.py
+++ htmlExtract.py
@@ -1,30 +1,48 @@
 # This file is to test out retrieving the URL from a dataset's XML file
 # and then using Boilerpy to grab all the text
 
-# TODO: Develop way to get continuously grab URL and output contents for multiple dataset files
+import os
+from urllib.error import HTTPError
 
 import lxml.etree as ET
 from boilerpy3 import extractors
-extractor = extractors.ArticleExtractor()
 
-def getUrl(dataset):
-    file = open(dataset)
-    tree = ET.parse(file)
+extractor = extractors.DefaultExtractor()
+path = os.getcwd() + '/'
+filelist = os.listdir(path)
+
+def getUrl(tree):
+    #file = open(dataset)
+    #tree = ET.parse(file)
     root = tree.getroot()
 
     # Find the URL which is an attribute (url) of the <Document> tag
     return root.attrib['url']
 
-def parseHtml(url, outputFile):
-    content = extractor.get_content_from_url(url)
-
-    file = open(outputFile, "a")
-    file.write(content)
-    file.close()
+def parseHtml(url,id,source, outputFile):
+    try:
+        content = extractor.get_content_from_url(url)
+        file = open(outputFile, "a", encoding="utf-8")
+        file.write(source + ": " + id + "\n" + content + "\n")
+        file.close()
+    except HTTPError:
+        file = open(outputFile, "a", encoding="utf-8")
+        file.write(source + ": " + id + "\n404: Not Found\n")
+        file.close
+    except ConnectionError:
+        print("Connection error")
 
 def main():
-    url = getUrl("0000001_1.xml")
-    parseHtml(url, "htmlOutput.txt")
+    for dataset in [f for f in os.listdir(path+'MedQuAD/') if f.endswith("QA") or f.endswith("XML")]:
+        print(dataset)
+        for xml_file in [f for f in os.listdir(path+'MedQuAD/' + dataset + '/') if f.endswith(".xml")]:
+            tree = ET.parse(os.path.join(path+'MedQuAD/' + dataset + '/', xml_file))
+            root = tree.getroot()
+            url = root.attrib['url']
+            id = root.attrib['id']
+            source = root.attrib['source']
+            print(source + ": " + id)
+            parseHtml(url, id, source, "htmlExtraction.txt")
 
 if __name__ == "__main__":
     main()
